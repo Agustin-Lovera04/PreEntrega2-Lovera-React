e79fe68b6ebd97892e1735aebddedf7e971367f9
Agustin-Lovera04 <agusslovera04@gmail.com>
2023-08-28 14:33:20 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"https://Agustin-Lovera04.github.io/PreEntrega2-Lovera-React"
+  plugins: [react()]
 })
