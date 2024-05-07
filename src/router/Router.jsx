4dd33874a46414acd23492c84c9e0e1848aeae0d
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-05-07 13:17:25 -0300

    deploy

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -17,7 +17,7 @@ const Router = () => {
             <BrowserRouter>
                 <NavBar />
                 <Routes>
-                    <Route path="/" element={<ItemListContainer />} />
+                    <Route path="/ProyectoFinal-Lovera-React/" element={<ItemListContainer />} />
                     <Route path="/category/:id" element={<ItemListContainer />} />
                     <Route path="/item/:id" element={<ItemDetailContainer />} />
                     <Route path="/cart" element={<CartView />} />
