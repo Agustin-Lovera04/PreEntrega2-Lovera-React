3160bf24855ab18109840ec761174bcfd760189d
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-05-07 13:01:21 -0300

    deploy

--- src/components/itemListContainer/index.jsx
+++ src/components/itemListContainer/index.jsx
@@ -50,7 +50,7 @@ const ItemListContainer = () => {
         <div>
           <ItemList articulos={articulos} id={id}/>
           <a href="https://w.app/W98juf" className="boton-flotante" target="_blank">
-            <img src="/public/imagenes/whatsapp (1).png" alt="btn whatsapp" />
+            <img src="../public/imagenes/whatsapp (1).png" alt="btn whatsapp" />
           </a>
         </div>
       )}
